f891be520d3057d16a0b50d3537d473ad4f629be
sharad-3131 <sanapsharad2002@gmail.com>
2025-04-22 19:14:59 +0530

    Deleted all old files

--- adc_driver.d
+++
@@ -1,8 +0,0 @@
-adc_driver.o: adc_driver.c
-adc_driver.o: C:\KeilARM\ARM\INC\Philips\LPC21XX.H
-adc_driver.o: types.h
-adc_driver.o: adc_defines.h
-adc_driver.o: defines.h
-adc_driver.o: types.h
-adc_driver.o: delay.h
-adc_driver.o: types.h
--- indicator_node.d
+++
@@ -1,8 +0,0 @@
-indicator_node.o: INDICATOR_NODE.c
-indicator_node.o: C:\KeilARM\ARM\INC\Philips\LPC21xx.h
-indicator_node.o: can.h
-indicator_node.o: types.h
-indicator_node.o: delay.h
-indicator_node.o: types.h
-indicator_node.o: defines.h
-indicator_node.o: types.h
